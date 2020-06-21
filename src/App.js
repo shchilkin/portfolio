ac440ee873b3eb92f8786430704a3a19de36ab7b
Aleksandr Shchilkin <crazyredkitten@icloud.com>
2020-06-21 20:05:04 +0300

    Added first (alpha) version of the website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import Logo from "./Logo";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="grid">
+          <nav className="navbar">
+              <div className="navbar-logo">
+                  <Logo/>
+              </div>
+              <div className="navbar-items">
+                  <a href="#work" className="navbar-item">Home</a>
+                  <a href="#projects" className="navbar-item">Projects</a>
+                  <a href="#skills" className="navbar-item">Skills</a>
+                  <a href="#" className="navbar-item">Contact</a>
+              </div>
+          </nav>
+          <section id="home" className="container">
+              <h1 className="display-1">
+                  Aleksandr <br/> <span className="accentText">Shchilkin</span>
+              </h1>
+              <h2 className="display-2">
+                  Web development & UI/UX Design
+              </h2>
+          </section>
+          <section id="work" className="container">
+              <h3 className="display-3">My <span className="accentText">work</span></h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque imperdiet scelerisque erat, a
+                  scelerisque eros sollicitudin cursus. Maecenas quis nunc ullamcorper, ultrices risus ac, efficitur
+                  tellus. Nunc pulvinar lectus nunc, sed tincidunt arcu ornare ut. Phasellus in pellentesque tortor.
+                  Morbi sed porta massa. Cras blandit vitae dolor non viverra. Nullam scelerisque sapien in eleifend
+                  consequat. Proin lacus lacus, blandit a nisl ut, maximus vestibulum libero.
+              </p>
+          </section>
+          <section id="skills" className="container">
+              <h3 className="display-3 text-right">My <span className="accentText">skills</span></h3>
+          </section>
+          <section id="projects" className="container">
+              <h3 className="display-3">My <span className="accentText">projects</span></h3>
+          </section>
+      </div>
   );
 }
 
---
+++ src/Logo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Logo = () => {
+    return(
+        <svg width="156px" height="52px" viewBox="0 0 117 39" version="1.1"
+             style={{fillRule:'evenodd',clipRule:'evenodd',strokeLinejoin:'round',strokeMiterlimit:2}}>
+            <circle cx="97.5" cy="19.5" r="19.5" style={{fill: "#330327"}}/>
+            <circle cx="78" cy="19.5" r="19.5" style={{fill: "#66074e"}}/>
+            <circle cx="58.5" cy="19.5" r="19.5" style={{fill: "#990a76"}}/>
+            <circle cx="39" cy="19.5" r="19.5" style={{fill: "#cc0e9d"}}/>
+            <circle cx="19.5" cy="19.5" r="19.5" style={{fill:"#ff11c4"}}/>
+        </svg>
+    )
+}
+
+export default Logo;
